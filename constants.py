1686208b1f2d7a331123fccabef4114a3a338a1a
Matthew Coleman <matthew.coleman@analog.com>
2024-11-24 14:17:34 +0100

    Fxied keyboard non responsiveness

---
+++ constants.py
@@ -0,0 +1,54 @@
+import cv2 as cv
+
+
+color_maps = {
+    "Jet": cv.COLORMAP_JET,
+    "Hot": cv.COLORMAP_HOT,
+    "Cool": cv.COLORMAP_COOL,
+    "Spring": cv.COLORMAP_SPRING,
+    "Summer": cv.COLORMAP_SUMMER,
+    "Autumn": cv.COLORMAP_AUTUMN,
+    "Winter": cv.COLORMAP_WINTER,
+    "Rainbow": cv.COLORMAP_RAINBOW,
+    "Ocean": cv.COLORMAP_OCEAN,
+    "Pink": cv.COLORMAP_PINK,
+    "HSV": cv.COLORMAP_HSV,
+    "Parula": cv.COLORMAP_PARULA,
+    "Magma": cv.COLORMAP_MAGMA,
+    "Inferno": cv.COLORMAP_INFERNO,
+    "Plasma": cv.COLORMAP_PLASMA,
+}
+
+display_resolutions = { "480x320":[480,320], 
+                        "640x480":[640,480],
+                        "800x600":[800,600],
+                        "1024x768":[1024,768],
+                        "1280x1024":[1280,1024],
+                        "1920x1080":[1920,1080]
+}
+
+display_interpolations = {
+    "Nearest": cv.INTER_NEAREST,
+    "Linear": cv.INTER_LINEAR,
+    "Cubic": cv.INTER_CUBIC,
+    "Area": cv.INTER_AREA,
+    "Lanczos4": cv.INTER_LANCZOS4    
+}
+
+sample_rates = {
+    "1 Hz": 1,
+    "2 Hz": 2,
+    "4 Hz": 3,
+    "8 Hz": 4,
+    "16 Hz": 5,
+    "32 Hz": 6,
+    "64 Hz": 7
+}
+
+baudrates = [
+    57600,
+    115200,
+    230400,
+    460800,
+    921600
+]
--- ir_cam_app.py
+++ ir_cam_app.py
@@ -23,60 +23,10 @@ import logging
 
 from ir_serial_reader import IRSerialReader
 
+from constants import *
+
 #config logging to terminal
 logging.basicConfig(level=logging.INFO)
-
-color_maps = {
-    "Jet": cv.COLORMAP_JET,
-    "Hot": cv.COLORMAP_HOT,
-    "Cool": cv.COLORMAP_COOL,
-    "Spring": cv.COLORMAP_SPRING,
-    "Summer": cv.COLORMAP_SUMMER,
-    "Autumn": cv.COLORMAP_AUTUMN,
-    "Winter": cv.COLORMAP_WINTER,
-    "Rainbow": cv.COLORMAP_RAINBOW,
-    "Ocean": cv.COLORMAP_OCEAN,
-    "Pink": cv.COLORMAP_PINK,
-    "HSV": cv.COLORMAP_HSV,
-    "Parula": cv.COLORMAP_PARULA,
-    "Magma": cv.COLORMAP_MAGMA,
-    "Inferno": cv.COLORMAP_INFERNO,
-    "Plasma": cv.COLORMAP_PLASMA,
-}
-
-display_resolutions = { "480x320":[480,320], 
-                        "640x480":[640,480],
-                        "800x600":[800,600],
-                        "1024x768":[1024,768],
-                        "1280x1024":[1280,1024],
-                        "1920x1080":[1920,1080]
-}
-
-display_interpolations = {
-    "Nearest": cv.INTER_NEAREST,
-    "Linear": cv.INTER_LINEAR,
-    "Cubic": cv.INTER_CUBIC,
-    "Area": cv.INTER_AREA,
-    "Lanczos4": cv.INTER_LANCZOS4    
-}
-
-sample_rates = {
-    "1 Hz": 1,
-    "2 Hz": 2,
-    "4 Hz": 3,
-    "8 Hz": 4,
-    "16 Hz": 5,
-    "32 Hz": 6,
-    "64 Hz": 7
-}
-
-baudrates = [
-    57600,
-    115200,
-    230400,
-    460800,
-    921600
-]
 
 
 class IRCamApp(tk.Tk):
@@ -224,12 +174,11 @@ class IRCamApp(tk.Tk):
         if not self.ir_serial_reader.is_alive():
             return
         
-        if not self.ir_serial_reader.rx_queue.empty():
-            data = self.ir_serial_reader.rx_queue.get_nowait()
-            if data is not None:
-                self._process_data(data)                    
+        data = self.ir_serial_reader.rx_queue.get(10)
+        if data is not None:
+            self._process_data(data)                    
 
-        self.after(100, self._read_data)
+        self.after(10, self._read_data)
 
 
     def _process_data(self, data):
